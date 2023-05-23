0a0c71d566641b9d599311f530a79e4f4b414152
Lucas Ferraz Martins <lucasuk89@gmail.com>
2023-05-23 10:24:58 +0200

    Imgs changes

--- src/data.js
+++ src/data.js
@@ -2,17 +2,17 @@
 export const products = [
     {
         id: 1,
-        img: "https://i.ibb.co/Xb1mw6P/apeparia-vercel-app.png",
+        img: "https://i.ibb.co/ZhHZ22t/screencapture-apeparia-la3h6fqzo-lucasuk89-vercel-app-2023-05-23-10-01-48.png",
         link: "https://apeparia-la3h6fqzo-lucasuk89.vercel.app",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/PmXFQkB/projeto-spotify-one-vercel-app.png",
+        img: "https://i.ibb.co/X4Fst4J/screencapture-projeto-spotify-one-vercel-app-2023-05-23-10-19-28.png",
         link: "https://projeto-spotify-one.vercel.app",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/vHMnGsP/restaurant-website-sigma-vercel-app.png",
+        img: "https://i.ibb.co/r6DFw7s/screencapture-restaurant-website-sigma-vercel-app-2023-05-23-10-21-13.png",
         link: "https://restaurant-website-sigma.vercel.app",
     },
     {
@@ -27,7 +27,7 @@ export const products = [
     },
     {
         id: 6,
-        img: "https://i.ibb.co/Tg9jSz3/organo-weld-vercel-app.png",
+        img: "https://i.ibb.co/ynLBZnS/screencapture-organo-weld-vercel-app-2023-05-23-10-23-03.png",
         link: "https://organo-weld.vercel.app",
     },
 ]
